f73f182a69518d0d60cf1c65898fd882d5c2284e
Christian Yepes Gomez <christianyepesgomez@gmail.com>
2022-04-26 07:48:14 +0200

    Deploy to host

--- AERData/AERData/spiders/AERSpider.py
+++ AERData/AERData/spiders/AERSpider.py
@@ -2,14 +2,19 @@ from ..items import *
 from platformshconfig import Config
 import psycopg2
 
-# config = Config()
-# database = config.credentials('postgresql')
+config = Config()
+database = config.credentials('postgresql')
+hostname = '127.0.0.1'
+username = 'main'
+password = 'main'
+database = 'main'
+port = '30000'
 
-hostname = 'postgresql'
-username = 'root'
-password = 'root'
-database = 'API_AER'
-port = '5432'
+# hostname = 'postgresql'
+# username = 'root'
+# password = 'root'
+# database = 'API_AER'
+# port = '5432'
 
 
 # Get Problems class
